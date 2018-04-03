43c74f0c40df0348a357a7fe7a6fe90153e8089f
Yuds <quinsyy@gmail.com>
2018-04-03 10:00:36 +0800

    Lab/Lec Button
    button resized.
    
    minor adjustment in homepage.

--- Admin/adminAddSchedulePage.php
+++ Admin/adminAddSchedulePage.php
@@ -136,11 +136,12 @@
 
 				<div class="col-sm-4">
 					<div class="form-group">
-						<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#lecturebtn">Lecture Room</button>
+					<h3>Add:</h3>
+						<button type="button" class="btn btn-info btn-block" data-toggle="collapse" data-target="#lecturebtn">Lecture Room</button>
 
 							<div id="lecturebtn" class="collapse">
 								<div class="form-group">
-								<h2></h2>
+								
 									<label for="timestartlec">Time Start:</label>
 	  								<select class="form-control" name="TimeStartLec" id="timestartlec">
 	  									<option>Select Start Time</option>
@@ -231,7 +232,8 @@
 
 				<div class="col-sm-4">
 					<div class="form-group">
-						<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#laboratorybtn">Laboratory Room</button>
+					<h3>Add:</h3>
+						<button type="button" class="btn btn-info btn-block" data-toggle="collapse" data-target="#laboratorybtn">Laboratory Room</button>
 
 							<div id="laboratorybtn" class="collapse">
 								<div class="form-group">
@@ -326,10 +328,12 @@
 				</div>
 			</div>
 
-			<div class="col-sm-3 pull-right">
+			<div class="col-sm-12">
+			<div class="pull-right">
 				<h1></h1>
 	 			<input type="submit" name="submit" class="btn btn-success"/>
 	   			<button type="button" class="btn btn-default">Clear All</button>
+			</div>
 			</div>
 		</form>
 
